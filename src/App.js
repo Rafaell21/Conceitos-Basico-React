0ea51f3948a9ade5825d544726cab35d47f26006
Rafael Silva <rafaell_21@hotmail.com>
2021-02-17 18:53:10 -0300

    Alterações nos metodos para adicionar e remover projetos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 import "./styles.css";
 
+
 function App() {
+
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories', {
+
+      "title": "Conceitos Basicos React",
+      "url": "https://github.com/Rafaell21/Conceitos-Basico-React.git",
+      "techs": [React, Node]
+
+    });
+
+    const repository = response.data
+    setRepository([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepository(repositories.filter(
+      repository => repository.id != id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(repository =>
+          <li key={repository.id}>{repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>)}
       </ul>
+      <ul><button onClick={handleAddRepository}>Adicionar</button></ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+
     </div>
   );
 }
